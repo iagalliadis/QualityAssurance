d8269e9eee89d8c32151cfa0cab6c2cd85586d97
iagalliadis <johnalias25@gmail.com>
2022-01-17 17:25:46 +0100

    unit test 1

--- scratch.js
+++ scratch.js
@@ -1,4 +1,12 @@
-test('#isNull, #isNotNull', function () {
-    assert.isNull(null, 'This is an optional error description - e.g. null is null');
-    assert.isNotNull(1, '1 is not null');
+
+const chai = require('chai');
+const assert = chai.assert;
+
+suite('Unit Tests', function () {
+    suite('Basic Assertions', function () {
+        test('#isNull, #isNotNull', function () {
+            assert.isNull(null, 'This is an optional error description - e.g. null is null');
+            assert.isNotNull(1, '1 is not null');
+        });
+    });
 });
